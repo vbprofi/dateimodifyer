41ab3fc903f872fae2e5a066691ce0b440df2fcb
Testgit <Testgit@gmx.de>
2018-10-14 10:38:15 +0200

    Neue Version

--- dateimodifyer/Form1.Designer.cs
+++ dateimodifyer/Form1.Designer.cs
@@ -44,25 +44,28 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(186, 159);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Dateiname";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(0, -2);
+            this.textBox1.Location = new System.Drawing.Point(93, 150);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
             this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(0, 150);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(89, 22);
             this.button1.TabIndex = 2;
             this.button1.Text = "Durchsuchen";
             this.button1.UseVisualStyleBackColor = true;
@@ -71,18 +74,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Location = new System.Drawing.Point(0, -2);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(234, 20);
+            this.label2.Size = new System.Drawing.Size(159, 13);
             this.label2.TabIndex = 3;
             this.label2.Text = "Zeitangabe Datum: TT.MM.JJJJ";
             // 
             // button2
             // 
             this.button2.AccessibleName = "Änderungsdatum ändern";
-            this.button2.Location = new System.Drawing.Point(8, 8);
+            this.button2.Location = new System.Drawing.Point(87, 96);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(82, 30);
             this.button2.TabIndex = 7;
             this.button2.Text = "Auf \'Zuletzt geändert\' anwenden";
             this.button2.UseVisualStyleBackColor = true;
@@ -95,33 +100,37 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(8, 8);
+            this.textBox2.Location = new System.Drawing.Point(0, 13);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
             this.textBox2.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 16);
+            this.label3.Location = new System.Drawing.Point(0, 47);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(233, 20);
+            this.label3.Size = new System.Drawing.Size(157, 13);
             this.label3.TabIndex = 5;
             this.label3.Text = "Zeitangabe Uhrzeit: HH:MM:SS";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(24, 24);
+            this.textBox3.Location = new System.Drawing.Point(0, 72);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
             this.textBox3.TabIndex = 6;
             // 
             // button3
             // 
             this.button3.AccessibleName = "Zugriffsdatum ändern";
-            this.button3.Location = new System.Drawing.Point(16, 16);
+            this.button3.Location = new System.Drawing.Point(173, 96);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(156, 28);
             this.button3.TabIndex = 8;
             this.button3.Text = "Auf \'letzter Zugriff\' anwenden";
             this.button3.UseVisualStyleBackColor = true;
@@ -130,9 +139,10 @@
             // button4
             // 
             this.button4.AccessibleName = "Erstellungsdatum ändern";
-            this.button4.Location = new System.Drawing.Point(24, 24);
+            this.button4.Location = new System.Drawing.Point(0, 96);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(83, 28);
             this.button4.TabIndex = 7;
             this.button4.Text = "Auf \'Erstellt am\' anwenden";
             this.button4.UseVisualStyleBackColor = true;
@@ -140,9 +150,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.ClientSize = new System.Drawing.Size(475, 343);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox3);
@@ -153,6 +163,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "DateiModifyer";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
